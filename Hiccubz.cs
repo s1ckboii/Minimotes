f2844b3c8b3dab60bcf996f39299c6317aaad0d2
s1ckboy <sickxedits@gmail.com>
2025-05-04 23:06:09 +0200

    Add project files.

---
+++ Hiccubz.cs
@@ -0,0 +1,454 @@
+﻿using Photon.Pun;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.AI;
+
+namespace AlienValuable;
+
+public class Hiccubz : MonoBehaviour
+{
+    private enum State
+    {
+        Idle,
+        Grabbed,
+        Notice,
+        Stashed,
+        Flee
+    }
+
+    public enum Emotion
+    {
+        Blank,
+        Happy,
+        Happy2,
+        Notice,
+        Sad,
+        Scared,
+        Angry,
+        Curious,
+        Hit,
+        Sleep,
+        Sleep2
+    }
+
+    private EnemyNavMeshAgent navMeshAgent;
+    private EnemyVision vision;
+    private PhotonView photonView;
+    private PhysGrabObject physGrabObject;
+
+    private PlayerAvatar playerAvatar;
+
+    private State currentState;
+    
+    private Quaternion horizontalRotationTarget = Quaternion.identity;
+    private Vector3 agentDestination;
+
+    private bool stateImpulse;
+    private bool hurtImpulse;
+
+    private float hurtLerp;
+    private float stateTimer;
+
+    private int hurtAmount;
+    private int defaultLayerMask;
+
+    private Coroutine emotionRoutine;
+
+    public AudioSource audioSource;
+    public AudioClip[] fleeSounds;
+    public AudioClip curious;
+    public AudioClip angry;
+    public AudioClip happy;
+    public AudioClip sleep;
+    public AudioClip sad;
+
+
+    public SkinnedMeshRenderer facialExpressions;
+    public SpringQuaternion horizontalRotationSpring;
+    public Material hurtableMaterial;
+    public Animator animator;
+    public AnimationCurve hurtCurve;
+    public Emotion emotion = Emotion.Blank;
+
+    private static readonly Emotion[] allEmotions = (Emotion[])System.Enum.GetValues(typeof(Emotion));
+
+    // I might need to add more / change some from trigger to boolean - I really didnt give this much thought yet
+
+    private static readonly int sitTrigger = Animator.StringToHash("idleSit");
+    private static readonly int grabbedTrigger = Animator.StringToHash("grabbed");
+    private static readonly int noticeTrigger = Animator.StringToHash("noticeAndStandUp");
+    private static readonly int fleeBool = Animator.StringToHash("runAway");
+    private static readonly int sleepBool = Animator.StringToHash("sleeping");
+    private void Awake()
+    {
+		navMeshAgent = GetComponent<EnemyNavMeshAgent>();
+        vision = GetComponent<EnemyVision>();
+        photonView = GetComponent<PhotonView>();
+        physGrabObject = GetComponent<PhysGrabObject>();
+        hurtAmount = Shader.PropertyToID("_ColorOverlayAmount");
+        hurtCurve = AssetManager.instance.animationCurveImpact;
+        defaultLayerMask = LayerMask.GetMask("Default");
+        UpdateState(State.Idle);
+
+        LevelPoint startHere = SemiFunc.LevelPointGet(base.transform.position, 0f, 15f);
+
+        navMeshAgent.Warp(startHere.transform.position);
+    }
+
+    private void Update()
+    {
+        if (SemiFunc.IsMasterClientOrSingleplayer())
+        {
+            switch (currentState)
+            {
+                case State.Idle:
+                    StateIdle();
+                    break;
+                case State.Grabbed:
+                    StateGrabbed();
+                    break;
+                case State.Notice:
+                    StateNotice();
+                    break;
+                case State.Flee:
+                    StateFlee();
+                    break;
+                case State.Stashed:
+                    StateStashed();
+                    break;
+            }
+        }
+        RotationLogic();
+        HurtEffect();
+    }
+
+    private void StateIdle()
+    {
+
+        // if spawned -> sit idle, do audio occasionally
+
+        if (stateImpulse)
+        {
+            animator.SetTrigger(sitTrigger);
+            stateImpulse = false;
+
+            SetFace(GetRandomEmotion(), 100f);
+
+            // if notices player AND is not in the cart or extraction point -> switch to StateNotice()
+
+            if (playerAvatar)
+            {
+                UpdateState(State.Notice);
+            }
+
+            // if grabbed -> switch to StateGrabbed()
+
+            if (physGrabObject.grabbed)
+            {
+                navMeshAgent.Disable(9999f);
+                UpdateState(State.Grabbed);
+            }
+        }
+    }
+
+    private void StateGrabbed()
+    {
+
+        // if grabbed -> do not move (but maybe do some animations, sounds)
+
+        if (stateImpulse)
+        {
+            animator.SetTrigger(grabbedTrigger);
+            stateImpulse = false;
+            stateTimer = 1f;
+
+            if (emotionRoutine != null)
+                StopCoroutine(emotionRoutine);
+            emotionRoutine = StartCoroutine(GrabbedEmotions());
+
+            // if she was put in the cart or in extraction point -> go to sleep
+            // NOT IN CART OR EXTRACTION POINT!!! <- still need to add as an &&s
+
+            if (!physGrabObject.grabbed)
+            {
+                if (InCartOrExtractionPoint())
+                {
+                    UpdateState(State.Stashed);
+                }
+
+                // if she was let go and not in the cart nor in extraction point -> flee the scene
+                // NOT IN CART OR EXTRACTION POINT!!! <- still need to add as an &&
+
+                else
+                {
+                    stateTimer -= Time.deltaTime;
+                    if (stateTimer <= 0f)
+                    {
+                        navMeshAgent.Enable();
+                        UpdateState(State.Notice);
+                    }
+                }
+            }
+        }
+    }
+
+    private void StateNotice()
+    {
+        if (stateImpulse)
+        {
+            animator.SetTrigger(noticeTrigger);
+            stateImpulse = false;
+            stateTimer = 1f;
+
+            SetFace(Emotion.Notice, 100f);
+        }
+        stateTimer -= Time.deltaTime;
+        if (stateTimer <= 0f)
+        {
+            UpdateState(State.Flee);
+        }
+    }
+
+    private void StateFlee()
+    {
+
+        // run away to a levelpoint far from the current location
+
+        if (stateImpulse)
+        {
+            animator.SetBool(fleeBool, true);
+            stateImpulse = false;
+            stateTimer = Random.Range(8f, 12f);
+
+            SetFace(Emotion.Scared, 100f);
+
+            LevelPoint levelPoint = SemiFunc.LevelPointGet(base.transform.position, 25f, 999f);
+            if (levelPoint != null &&
+                NavMesh.SamplePosition(levelPoint.transform.position + Random.insideUnitSphere * 3f, out var hit, 5f, -1) &&
+                Physics.Raycast(hit.position, Vector3.down, 5f, defaultLayerMask))
+            {
+                agentDestination = hit.position;
+            }
+            else
+            {
+                UpdateState(State.Idle);
+                return;
+            }
+        }
+        else
+        {
+            navMeshAgent.SetDestination(agentDestination);
+
+            // when she ran away to her chosen destination -> go back to idle
+
+            stateTimer -= Time.deltaTime;
+            if (stateTimer <= 0f)
+            {
+                UpdateState(State.Idle);
+            }
+
+            // if shes grabbed -> updatestate to StateGrabbed()
+            if (physGrabObject.grabbed)
+            {
+                stateTimer = 0f;
+                navMeshAgent.Disable(9999f);
+                UpdateState(State.Grabbed);
+            }
+        }
+    }
+
+    private void StateStashed()
+    {
+        if (stateImpulse)
+        {
+            stateImpulse = false;
+            animator.SetBool(sleepBool, true);
+
+            SetFace(Emotion.Sleep, 100f);
+
+            // if in extraction point or in cart -> dont move (maybe do some audios)
+
+            if (!InCartOrExtractionPoint())
+            {
+                navMeshAgent.Enable();
+                UpdateState(State.Notice);
+            }
+        }
+    }
+
+    private bool InCartOrExtractionPoint()
+    {
+
+        // if in extraction point or cart(?)-> turn bool true else false
+
+        return RoundDirector.instance.dollarHaulList.Contains(base.gameObject);
+    }
+
+    private void RotationLogic()
+    {
+        if (navMeshAgent.AgentVelocity.normalized.magnitude > 0.1f)
+        {
+            horizontalRotationTarget = Quaternion.LookRotation(navMeshAgent.AgentVelocity.normalized);
+            horizontalRotationTarget.eulerAngles = new Vector3(0f, horizontalRotationTarget.eulerAngles.y, 0f);
+            horizontalRotationSpring.speed = 15f;
+            horizontalRotationSpring.damping = 0.8f;
+        }
+        base.transform.rotation = SemiFunc.SpringQuaternionGet(horizontalRotationSpring, horizontalRotationTarget);
+    }
+    public void OnVision()
+    {
+        playerAvatar = vision.onVisionTriggeredPlayer;
+        if (GameManager.Multiplayer())
+        {
+            photonView.RPC("UpdatePlayerTargetRPC", RpcTarget.All, playerAvatar.photonView.ViewID);
+        }
+    }
+
+    private void SetFace(Emotion emotionState, float weight)
+    {
+        ResetFace();
+
+        emotion = emotionState;
+        facialExpressions.SetBlendShapeWeight((int)emotionState, Mathf.Clamp(weight, 0f, 100f));
+
+        EmotionAudio(emotionState);
+    }
+
+    private void ResetFace()
+    {
+        foreach (Emotion emotionState in allEmotions)
+        {
+            facialExpressions.SetBlendShapeWeight((int)emotionState, 0f);
+        }
+    }
+
+    public void OnHurt()
+    {
+        hurtImpulse = true;
+        if (emotionRoutine != null)
+        {
+            StopCoroutine(emotionRoutine);
+        }
+        emotionRoutine = StartCoroutine(HurtEmotions());
+    }
+
+    private void HurtEffect()
+    {
+        if (hurtImpulse)
+        {
+            hurtLerp += 2.5f * Time.deltaTime;
+            hurtLerp = Mathf.Clamp01(hurtLerp);
+            hurtableMaterial.SetFloat(hurtAmount, hurtCurve.Evaluate(hurtLerp));
+            if (hurtLerp >= 1f)
+            {
+                    hurtLerp = 0f;
+                    hurtImpulse = false;
+                    hurtableMaterial.SetFloat(hurtAmount, 0f);
+            }
+        }
+    }
+
+    private void UpdateState(State state)
+    {
+        currentState = state;
+        stateImpulse = true;
+        animator.SetBool(sleepBool, false);
+        animator.SetBool(fleeBool, false);
+        stateTimer = 0f;
+        if (GameManager.Multiplayer())
+        {
+            photonView.RPC("UpdateStateRPC", RpcTarget.All, currentState);
+        }
+        else
+        {
+            UpdateStateRPC(currentState);
+        }
+    }
+
+    private Emotion GetRandomEmotion()
+    {
+        return ((Emotion[])[
+            Emotion.Happy,
+            Emotion.Happy2,
+            Emotion.Curious,
+        ])[Random.Range(0, ((Emotion[])[
+            Emotion.Happy,
+            Emotion.Happy2,
+            Emotion.Curious,
+        ]).Length)];
+    }
+
+    private IEnumerator GrabbedEmotions()
+    {
+
+        foreach (Emotion e in (Emotion[])[
+            Emotion.Scared,
+            Emotion.Sad,
+            Emotion.Angry,
+            Emotion.Blank,
+            Emotion.Curious
+        ])
+        {
+            SetFace(e, 100f);
+            yield return new WaitForSeconds(Random.Range(3f, 4f));
+        }
+
+        SetFace(Emotion.Blank, 0f);
+    }
+
+    private IEnumerator HurtEmotions()
+    {
+        SetFace(Emotion.Hit, 100f);
+        yield return new WaitForSeconds(0.4f);
+
+        SetFace(Emotion.Angry, 100f);
+    }
+
+    private void EmotionAudio(Emotion emotionState)
+    {
+        switch (emotionState)
+        {
+            case Emotion.Angry:
+                audioSource.PlayOneShot(angry);
+                break;
+            case Emotion.Curious:
+                audioSource.PlayOneShot(curious);
+                break;
+            case Emotion.Happy:
+                audioSource.PlayOneShot(happy);
+                break;
+            case Emotion.Happy2:
+                audioSource.PlayOneShot(happy);
+                break;
+            case Emotion.Sleep:
+                audioSource.PlayOneShot(sleep);
+                break;
+            case Emotion.Sleep2:
+                audioSource.PlayOneShot(sleep);
+                break;
+            case Emotion.Sad:
+                audioSource.PlayOneShot(sad);
+                break;
+        }
+    }
+
+    [PunRPC]
+    private void UpdateStateRPC(State state)
+    {
+        currentState = state;
+    }
+
+    [PunRPC]
+    private void UpdatePlayerTargetRPC(int viewID)
+    {
+        foreach (PlayerAvatar player in SemiFunc.PlayerGetList())
+        {
+            if (player.photonView.ViewID == viewID)
+            {
+                playerAvatar = player;
+                break;
+            }
+        }
+    }
+}
---
+++ Plugin.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections.Generic;
+using System.IO;
+using BepInEx;
+using REPOLib.Modules;
+using UnityEngine;
+
+namespace AlienValuable;
+
+[BepInPlugin(MyPluginInfo.PLUGIN_GUID, MyPluginInfo.PLUGIN_NAME, MyPluginInfo.PLUGIN_VERSION)]
+[BepInDependency(REPOLib.MyPluginInfo.PLUGIN_GUID, BepInDependency.DependencyFlags.HardDependency)]
+internal class Plugin : BaseUnityPlugin
+{
+    private void Awake()
+    {
+        string pluginFolderPath = Path.GetDirectoryName(Info.Location);
+        string assetBundleFilePath = Path.Combine(pluginFolderPath, "hiccubz");
+
+        AssetBundle assetBundle = AssetBundle.LoadFromFile(assetBundleFilePath);
+
+        if (assetBundle == null )
+        {
+            Logger.LogError("Failed to load Hiccubz assetbundle");
+            return;
+        }
+
+        List<string> list = ["Valuables - Generic"];
+
+        GameObject Piccubz = assetBundle.LoadAsset<GameObject>("Valuable Hiccubz");
+        Valuables.RegisterValuable(Piccubz, list);
+    }
+}
